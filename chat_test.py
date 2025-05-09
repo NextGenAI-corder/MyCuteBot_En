90f37b80edee5a976aefe956e43a5635bf95243f
CCurata <urata@mtd.biglob.ne.jp>
2025-05-09 22:33:04 +0900

    changed by English version

--- chat_test.py
+++ chat_test.py
@@ -1,17 +1,13 @@
 import os
 from openai import OpenAI
 
-def get_chatgpt(): #ChatGPTの応答を得る関数
-    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY")) #環境変数からAPIきーを取得
-
+def get_chatgpt(): #This function will get a ChatGPT response.
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY")) # Obtain the API key from the environment variable.
     response = client.chat.completions.create(
         model="gpt-4o",
-        messages=[{"role": "user", "content": "こんにちは！ゴールデンウィークはどこか行きましたか？何か日本で事故とかありましたか？"}],#プロンプト
-        max_tokens=300
-        # Limit the maximum number of tokens in the response (shorter replies)
+        messages=[{"role": "user", "content": "hello"}] ,# Set the prompt.
+        max_tokens= 30 # Limit the maximum number of tokens in a response.
     )
-
     print(response.choices[0].message.content)
-
 if __name__ == "__main__":
     get_chatgpt()
